8421e1264fda1c0e371f133f118c30b234c29035
Navod Senarathne <navod228@gmail.com>
2023-11-10 07:59:13 +0530

    Directories

---
+++ app/controllers/home.php
@@ -0,0 +1,11 @@
+<?
+
+class home extends controller
+{
+    public function index()
+    {
+        echo "home.php/index";
+    }
+}
+
+?>
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once '../app/init.php';
+
+$app = new app;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Management System | University of Colombo</title>
+</head>
+<body>
+    <h1>Inventory Management System / Home</h1>
+</body>
+</html>
