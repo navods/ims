46351e15f0c8602b8c53a03c6cd9369455cb6784
Navod Senarathne <88228646+navods@users.noreply.github.com>
2023-11-19 19:58:56 +0530

    After Katina

---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>Dashboard | IMS</title>
+</head>
+<body>
+    <?php include "include/navbar.php" ?>
+    <div class="bread">Breadcrum</div><br>
+    <div class="menu">
+        <div class="menuItem"><a href="inventoryMenu.php">Inventory Manager</a></div>
+        <div class="menuItem"><a href="reportsMenu.php">Reports</a></div>
+        <div class="menuItem"><a href="sectionMenu.php">Section Manager</a></div>
+        <div class="menuItem"><a href="permissionMenu.php">User Permission Manager</a></div>
+        <div class="menuItem"><a href="profileMenu.php">Profile</a></div>
+    </div>
+</body>
+</html>
---
+++ include/navbar.php
@@ -0,0 +1,8 @@
+<header>
+    <img class="navLogo" src="img/shortLogoW.svg">
+    <nav class="navText">
+        <div><a href="dashboard.php">Dashboard</a></div>
+        <div><a href="profile.php">Profile</a></div>
+        <div><a href="signout.php">SignOut</a></div>
+    </nav>
+</header>
